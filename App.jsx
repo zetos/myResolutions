af5f6bf2947d1ead3f56f93ce0e1a473f7c0cdb5
Zetos <cesar_manoel@hotmail.com>
2016-07-14 19:40:54 -0300

    routes added

--- App.jsx
+++ App.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
+export default class App extends React.Component {
 	render(){
 		return (
 			<h1>Hello World</h1>
 			)
 	}
-}
-
-if(Meteor.isClient) {
-	Meteor.startup(function(){
-		ReactDOM.render(<App />, document.getElementById("render-target"))
-	})
 }
