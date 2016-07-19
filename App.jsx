1bfd07249df1f5a92fa0d8589471a61bea18004c
Zetos <cesar_manoel@hotmail.com>
2016-07-18 23:36:00 -0300

    inserting resolution to database

--- App.jsx
+++ App.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+Resolutions = new Mongo.Collection("resolutions");
+
 export default class App extends React.Component {
+
+	addResolution(event){
+		event.preventDefault();
+		const text = this.refs.resolution.value.trim();
+
+		Resolutions.insert({
+			text: text,
+			complete: false,
+			createdAt: new Date()
+		});
+
+		
+		this.refs.resolution.value = "";
+	}
+
 	render(){
 		return (
-			<h1>Hello World</h1>
-			)
+			<div>
+				<h1>My Resolutions</h1>
+				<form className="new-resoltuion" onSubmit={this.addResolution.bind(this)}>
+					<input 
+					type="text" 
+					ref="resolution" 
+					placeholder="Finish React Meteor Series" />
+				</form>
+			</div>
+		)
 	}
 }
