b793d40117d108295df702c072aadb1bb4c5cb49
Zetos <cesar_manoel@hotmail.com>
2016-07-20 18:08:04 -0300

    remove and update methods implemented

--- client/resolutions/ResolutionSingle.jsx
+++ client/resolutions/ResolutionSingle.jsx
@@ -1,11 +1,29 @@
 import React, {Component} from 'react';
 
 export default class ResolutionSingle extends Component {
+
+	toggleChecked() {
+		console.log(this);
+		Meteor.call('toggleResolution', this.props.resolution._id, this.props.resolution.complete);
+	}
+
+	deleteResolution() {
+		console.log(this);
+		Meteor.call('deleteResolution', this.props.resolution._id);
+	}
+
 	render() {
-		return(
+		return (
 			<li>
+				<input type="checkbox"
+				readOnly={true}
+				checked={this.props.resolution.complete}
+				onClick={this.toggleChecked.bind(this)} />
 				{this.props.resolution.text}
-				{this.props.resolution.complete.toString()}
+				<button className="btn-cancel"
+					onClick={this.deleteResolution.bind(this)}>
+						&times;
+					</button>
 			</li>
 			)
 	}
--- server/methods.js
+++ server/methods.js
@@ -5,5 +5,13 @@ Meteor.methods({
 			complete: false,
 			createdAt: new Date()
 		});
+	},
+	toggleResolution(id, status) {
+		Resolutions.update(id, {
+			$set: {complete: !status}
+		});
+	},
+	deleteResolution(id) {
+		Resolutions.remove(id);
 	}
 });
