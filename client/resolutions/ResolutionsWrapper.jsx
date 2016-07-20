9c2cbc4ff9c730d4c3b528b9b7e27ff6a3459eee
Zetos <cesar_manoel@hotmail.com>
2016-07-20 02:30:46 -0300

    looping over data

--- client/resolutions/ResolutionsWrapper.jsx
+++ client/resolutions/ResolutionsWrapper.jsx
@@ -13,17 +13,16 @@ export default class ResolutionsWrapper extends TrackerReact(React.Component) {
 	}
 
 	render(){
-		let res = this.resolutions();
-		if(res.length < 1) {
-			return ( <div>Loading..</div> )
-		}
 
 		return (
 			<div>
 				<h1>My Resolutions</h1>
 				<ResolutionsForm />
-				<ul>
-					<ResolutionSingle resolution={res[0]} />
+				<ul className="resolutions">
+					{this.resolutions().map( (resolution)=>{
+						return <ResolutionSingle key={resolution._id} resolution={resolution} />
+					})}
+					
 				</ul>
 			</div>
 		)
