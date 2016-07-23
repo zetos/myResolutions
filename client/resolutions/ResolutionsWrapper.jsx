7a331a2c575b3da80300144111bc95996482bd21
Zetos <cesar_manoel@hotmail.com>
2016-07-23 17:25:32 -0300

    ReactCSSTransitionGroup added for css animation

--- client/resolutions/ResolutionsWrapper.jsx
+++ client/resolutions/ResolutionsWrapper.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import TrackerReact from 'meteor/ultimatejs:tracker-react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 import ResolutionsForm from './ResolutionsForm.jsx';
 import ResolutionSingle from './ResolutionSingle.jsx';
@@ -31,12 +32,16 @@ export default class ResolutionsWrapper extends TrackerReact(React.Component) {
 			<div>
 				<h1>My Resolutions - {Session.get('test')}</h1>
 				<ResolutionsForm />
-				<ul className="resolutions">
+				<ReactCSSTransitionGroup
+					component="ul"
+					className="resolutions"
+					transitionName="resolutionLoad"
+					transitionEnterTimeout={600}
+					transitionLeaveTimeout={400}>
 					{this.resolutions().map( (resolution)=>{
 						return <ResolutionSingle key={resolution._id} resolution={resolution} />
 					})}
-					
-				</ul>
+				</ReactCSSTransitionGroup>
 			</div>
 		)
 	}
